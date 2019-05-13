c7dc90191255ca091a5817c80aa0a5947ba4fec0
Eric Majerus <ackmanx@users.noreply.github.com>
2019-05-13 14:59:55 -0500

    Using passport to authenticate with spotify instead of homebrew from spotify tutorial

--- app.js
+++ app.js
@@ -3,37 +3,59 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport')
 
 const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
-// view engine setup
+// ---------------------------------------------------------------------------------
+// Engine setup
+// ---------------------------------------------------------------------------------
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+// ---------------------------------------------------------------------------------
+// Middleware
+// ---------------------------------------------------------------------------------
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+
+// ---------------------------------------------------------------------------------
+// Routes
+// ---------------------------------------------------------------------------------
 app.use('/', indexRouter);
-app.use('/user', userRouter);
+app.use('/auth', authRouter);
 
-// catch 404 and forward to error handler
+
+// ---------------------------------------------------------------------------------
+// Error handling
+// ---------------------------------------------------------------------------------
+
+//If we made it this far, none of our routes were triggered, so it is a 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
---
+++ routes/auth.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport')
+const SpotifyStrategy = require('passport-spotify').Strategy
+
+/*
+ * Spotify Developer Dashboard
+ * https://developer.spotify.com/dashboard/applications/e466e7a286954e13b501d8e7bc4669cc
+ */
+
+const clientID = 'e466e7a286954e13b501d8e7bc4669cc';
+const clientSecret = '76f2fd5049574e09899dcf84810e5fef'; //this will live in heroku config so it isn't exposed via git, but until then store it here and regen a new key later
+const callbackURL = 'http://localhost:3000/auth/callback'; //where to be sent by spotify after logging in. this must be whitelisted in spotify developer dashboard first
+
+passport.use(
+    new SpotifyStrategy(
+        {
+            clientID,
+            clientSecret,
+            callbackURL,
+        },
+        function (accessToken, refreshToken, expires_in, profile, done) {
+            process.nextTick(() => done(null, profile));
+        }
+    )
+);
+
+// ---------------------------------------------------------------------------------
+// REST
+// ---------------------------------------------------------------------------------
+const strategy = 'spotify'
+
+router.get('/login', passport.authenticate(strategy, {
+    scope: ['user-read-email', 'user-read-private'],
+    showDialog: true
+}), function () {
+    //Spotify redirects the request, so this handler will never be called
+});
+
+router.get('/callback', passport.authenticate(strategy, {failureRedirect: '/login#fail'}),
+    function success(req, res) {
+        res.redirect('/#blah');
+    }
+);
+
+module.exports = router;
