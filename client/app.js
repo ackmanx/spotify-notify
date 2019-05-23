72a7802808a44df5721fa8638a34880a74071bce
Eric Majerus <ackmanx@users.noreply.github.com>
2019-05-22 23:12:35 -0500

    added context. now need to give it state so i can access update my context from other components

--- client/app.js
+++ client/app.js
@@ -1,16 +1,37 @@
-import React, { useEffect } from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import {AppContext} from './context'
 
-export const App = () => {
+export class App extends React.Component {
+    constructor(props) {
+        super(props)
+        // this.state = {
+        //     albums: {},
+        //     setAlbums: this.setAlbums.bind(this)
+        // }
 
-    //hook callback cannot be async, so React recommends making an async function within and invoking immediately
-    useEffect(() => {
         async function init() {
             const response = await fetch('/api/get-new-albums')
             const newAlbums = await response.json()
-            console.log(newAlbums);
         }
-        init()
-    }, [])
 
-    return <h2>yo</h2>
+        init()
+    }
+
+    // setAlbums(albums) {
+    //     this.setState({albums})
+    // }
+
+    render() {
+        return (
+            <AppContext.Provider value={'hello'}>
+                <SomethingElse />
+            </AppContext.Provider>
+        )
+    }
+}
+
+class SomethingElse extends React.Component {
+    render() {
+        return <AppContext.Consumer>{context => context}</AppContext.Consumer>
+    }
 }
