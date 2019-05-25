6d6d7664922e8456f0b3bc755d2fe4ec0b3db57b
Eric Majerus <ackmanx@users.noreply.github.com>
2019-05-25 17:07:06 -0500

    added cache check and uncommented refresh code. will save refresh result into db next and cache will pull from there

--- routes/api.js
+++ routes/api.js
@@ -38,52 +38,50 @@ router.get('/get-artist-albums', ensureAuthenticated, async function (req, res) 
 })
 
 router.get('/get-new-albums', ensureAuthenticated, async function (req, res) {
-    const userSeenAlbums = db.get(req.session.user.id) || []
+    if (req.query.refresh !== 'true') {
+        return res.json(require('../mock/api/get-new-albums--cached'))
+    }
+
     let body = {}
+    const userSeenAlbums = db.get(req.session.user.id) || []
 
-    res.json(require('../mock/api/get-new-albums'))
+    const followedArtistsFromSpotify = await spotifyAPI(req, '/me/following?type=artist&limit=50')
+    followedArtistsFromSpotify.artists.items.forEach(artist =>
+        body[artist.id] = {
+            id: artist.id,
+            name: artist.name,
+        }
+    )
 
-    // const followedArtistsFromSpotify = await spotifyAPI(req, '/me/following?type=artist&limit=50')
-    // followedArtistsFromSpotify.artists.items.forEach(artist =>
-    //     body[artist.id] = {
-    //         id: artist.id,
-    //         name: artist.name,
-    //     }
-    // )
+    const allAlbumsPromises = []
 
-    //todo majerus: see if i can batch get albums for artists
+    for (let artistId in body) {
+        //todo majerus: see if i can batch get albums for artists
+        allAlbumsPromises.push(spotifyAPI(req, `/artists/${artistId}/albums?include_groups=album,single&market=US&limit=50`))
+    }
 
-    //todo majerus: this is finished, but commented out so I don't make 200 requests to spotify at the same time
-    //todo majerus: fake the body request above to include only a couple artists and it should be okay
+    const allAlbumsFollowedArtists = await Promise.all(allAlbumsPromises)
+    allAlbumsFollowedArtists.forEach(allAlbumsForSingleArtist => {
+        const [, artistId] = allAlbumsForSingleArtist.href.match(/artists\/(.+)\/albums/)
 
-    // const allAlbumsPromises = []
-    //
-    // for (let artistId in body) {
-    //     allAlbumsPromises.push(spotifyAPI(req, `/artists/${artistId}/albums?include_groups=album,single&market=US&limit=50`))
-    // }
-    //
-    // const allAlbumsFollowedArtists = await Promise.all(allAlbumsPromises)
-    // allAlbumsFollowedArtists.forEach(allAlbumsForSingleArtist => {
-    //     const [, artistId] = allAlbumsForSingleArtist.href.match(/artists\/(.+)\/albums/)
-    //
-    //     let albums = body[artistId].albums
-    //     if (!Array.isArray(albums)) albums = []
-    //
-    //     allAlbumsForSingleArtist.items.forEach(album => {
-    //         if (userSeenAlbums.includes(album.id)) return
-    //
-    //         albums.push({
-    //             id: album.id,
-    //             name: album.name,
-    //             url: album.external_urls.spotify,
-    //             coverArt: album.images[1].url, //response always has 3 images of diff sizes, and I always want the middle one
-    //         })
-    //     })
-    //
-    //     body[artistId].albums = albums
-    // })
+        let albums = body[artistId].albums
+        if (!Array.isArray(albums)) albums = []
 
-    // res.json(body)
+        allAlbumsForSingleArtist.items.forEach(album => {
+            if (userSeenAlbums.includes(album.id)) return
+
+            albums.push({
+                id: album.id,
+                name: album.name,
+                url: album.external_urls.spotify,
+                coverArt: album.images[1].url, //response always has 3 images of diff sizes, and I always want the middle one
+            })
+        })
+
+        body[artistId].albums = albums
+    })
+
+    res.json(body)
 })
 
 module.exports = router
