7945df69b49a3c3f2e2c21907af4ef1861f4140c
Eric Majerus <ackmanx@users.noreply.github.com>
2019-06-02 22:41:28 -0500

    added album overlay

--- client/components/album/album.js
+++ client/components/album/album.js
@@ -1,14 +1,31 @@
 import './album.css'
-import React from 'react'
+import React, {useState} from 'react'
 
 export const Album = ({album: {id, name, url, coverArt}, artist}) => {
     const artistAlbumName = `${artist} - ${name}`
 
+    const [hover, setHover] = useState(false);
+
+    const toggleOverlay = () => setHover(!hover)
+
     return (
-        <>
-            <a href={url} target='_blank'>
-                <img src={coverArt} title={artistAlbumName} alt={artistAlbumName}/>
-            </a>
-        </>
+        <div className='album'>
+            <img className='album-cover-art'
+                 src={coverArt}
+                 title={artistAlbumName}
+                 alt={artistAlbumName}
+                 onMouseEnter={toggleOverlay}/>
+
+            {hover && (
+                <div className='album-overlay' onMouseLeave={toggleOverlay}>
+                    <a className='spotify-link' href={url} target='_blank'>
+                        link
+                    </a>
+                    <a className='mark-as-seen' href='#' onClick={() => console.log('something')}>
+                        mark
+                    </a>
+                </div>
+            )}
+        </div>
     )
 }
