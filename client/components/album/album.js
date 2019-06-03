0d1cf9c68409973aa77a58ad45ad1e9e26f80eb4
Eric Majerus <ackmanx@users.noreply.github.com>
2019-06-02 23:02:53 -0500

    styled overlay

--- client/components/album/album.js
+++ client/components/album/album.js
@@ -19,10 +19,14 @@ export const Album = ({album: {id, name, url, coverArt}, artist}) => {
             {hover && (
                 <div className='album-overlay' onMouseLeave={toggleOverlay}>
                     <a className='spotify-link' href={url} target='_blank'>
-                        link
+                        <div className='overlay-link-container'>
+                            <img src='spotify-icon.png' alt='spotify logo'/>
+                        </div>
                     </a>
                     <a className='mark-as-seen' href='#' onClick={() => console.log('something')}>
-                        mark
+                        <div className='overlay-link-container'>
+                            <img src='mark-as-seen.png' alt='mark as seen'/>
+                        </div>
                     </a>
                 </div>
             )}
