bcb40c7cca7b7e510cfa04df50804bd296b9bfea
Eric Majerus <ackmanx@users.noreply.github.com>
2019-08-04 12:39:53 -0500

    css tweaks

--- client/components/artist/artist.js
+++ client/components/artist/artist.js
@@ -5,6 +5,7 @@ import {Album} from '../album/album'
 export const Artist = ({name, albums}) => {
     return (
         <div className='artist-group'>
+            <div className='artist-name'>{name}</div>
             {albums.map(album => <Album key={album.id} artist={name} album={album}/>)}
         </div>
     )
