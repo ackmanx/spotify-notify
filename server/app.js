3957254920c8268a10231719c0e9b4ae88a52c8f
Eric Majerus <ackmanx@users.noreply.github.com>
2019-08-28 18:49:18 -0500

    set a cache maxAge so it isn't the default zero

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, '..', 'public'), {maxAge: 31536000}))
 app.use(compression())
 
 app.use(session({
