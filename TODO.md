307f6c638d77ad862b2ed6159d643479b3e07af7
Eric Majerus <ackmanx@users.noreply.github.com>
2019-09-01 22:20:21 -0500

    adding artist in bulk doesn't send duplicates

--- TODO.md
+++ TODO.md
@@ -15,8 +15,6 @@ For local development, I'll need to keep the secret handy outside of git, maybe 
 
 ### Every Album instance on the page renders when you ghost an item
 
-### Clicking an artist to mark all albums as seen stacks and sends duplicates
-
 ### `totalNewAlbums` is not being populated correctly, or is not being updated when the cache is read
     * If a user marks an album as seen, but doesn't refresh. That should reduce `totalNewAlbums` still
     * The current problem is that I'm iterating over artists, thinking it's albums
--- client/redux/reducers/app.js
+++ client/redux/reducers/app.js
@@ -23,23 +23,20 @@ export function app(state = initialState, action = {}) {
         }
 
         case UPDATE_SEEN_ALBUMS: {
+            let markedAsSeen = action.albumId ? [action.albumId] : action.albumIds
+
             let seenAlbums = state.seenAlbums.slice()
 
-            //todo: be nice to add albumId to an array, and then operate on it the same as when passed an array
-            //todo: also need to prevent duplicates from being sent
-            if (action.albumId) {
-                const seenAlbumIndex = seenAlbums.indexOf(action.albumId)
+            markedAsSeen.forEach(albumId => {
+                const seenAlbumIndex = seenAlbums.indexOf(albumId)
 
                 if (seenAlbumIndex !== -1) {
                     seenAlbums.splice(seenAlbumIndex, 1)
                 }
                 else {
-                    seenAlbums.push(action.albumId)
+                    seenAlbums.push(albumId)
                 }
-            }
-            else {
-                seenAlbums = seenAlbums.concat(action.albumIds)
-            }
+            })
 
             return {...state, seenAlbums}
         }
