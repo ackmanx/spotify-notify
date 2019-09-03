bf831d5dacad4a9224c35b5bd94f16d894c7553e
Eric Majerus <ackmanx@users.noreply.github.com>
2019-09-03 16:47:46 -0500

    styling yay

--- client/components/artist/artist.js
+++ client/components/artist/artist.js
@@ -8,7 +8,13 @@ import {markArtistAsSeen} from '../../redux/actions/seen-albums'
 const _Artist = props => {
     const {artist, markArtistAsSeen} = props
 
-    if (!artist.albums.length) {
+    const albums = artist.albums.filter(album => album.type === 'album')
+    const singles = artist.albums.filter(album => album.type === 'single')
+
+    const hasAlbums = !!albums.length
+    const hasSingles = !!singles.length
+
+    if (!hasAlbums && !hasSingles) {
         return null
     }
 
@@ -16,23 +22,21 @@ const _Artist = props => {
 
     const toggleOverlay = () => setHover(!hover)
 
-    const albums = artist.albums.filter(album => album.type === 'album')
-    const singles = artist.albums.filter(album => album.type === 'single')
 
     return (
         <div className='artist-group'>
             <div className='artist-name-container' onMouseEnter={toggleOverlay} onMouseLeave={toggleOverlay}>
-                <div className='artist-name' onClick={() => markArtistAsSeen(artist.id)}>{artist.name}</div>
+                <h2 className='artist-name' onClick={() => markArtistAsSeen(artist.id)}>{artist.name}</h2>
                 {hover && <img src='album-actions/ghost.png' alt='mark as seen'/>}
             </div>
 
-            {!!albums.length && <>
-                <div className='album-group-title'>Albums</div>
+            {hasAlbums && <>
+                <h3 className='album-group-title'>Albums</h3>
                 {albums.map(album => <Album key={album.id} artistName={artist.name} album={album}/>)}
             </>}
 
-            {!!singles.length && <>
-                <div className='album-group-title'>Singles</div>
+            {hasSingles && <>
+                <h3 className='album-group-title'>Singles</h3>
                 {singles.map(album => <Album key={album.id} artistName={artist.name} album={album}/>)}
             </>}
         </div>
