c8225182cbbf47697a2a0aab2ed3c1d718defaae
Eric Majerus <ackmanx@users.noreply.github.com>
2019-09-03 18:08:57 -0500

    more css tweaks for mobile

--- client/components/album/placeholder.js
+++ client/components/album/placeholder.js
@@ -1,12 +1,6 @@
+import './placeholder.css'
 import React from 'react'
 
-const styles = {
-    width: 300,
-    height: 300,
-    border: '2px dashed black',
-    boxSizing: 'border-box'
-}
-
 export const Placeholder = () => (
-    <div style={styles}/>
+    <div className='image-placeholder'/>
 )
