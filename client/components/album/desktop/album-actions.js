da7e390803538e735dae06ecb5483a9011fd6e55
Eric Majerus <ackmanx@users.noreply.github.com>
2019-09-13 17:21:49 -0500

    cooler hover on album actions

--- client/components/album/desktop/album-actions.js
+++ client/components/album/desktop/album-actions.js
@@ -11,24 +11,18 @@ export const _DesktopAlbumActions = props => {
         <div className='desktop-album-actions'>
             <div className='desktop-album-actions-2'>
                 <div className='desktop-actions-container'>
-                    <div className='desktop-album-action-button'>
-                        <a className='desktop-album-action-spotify' href={album.spotifyUri}>
-                            <img src='album-actions/spotify.png' alt=''/> Open in Spotify App
-                        </a>
-                    </div>
-                    <div className='desktop-album-action-button'>
-                        <a className='desktop-album-action-spotify' href={album.spotifyWebPlayerUrl}>
-                            <img src='album-actions/google-chrome.png' alt=''/> Open in Spotify Web
-                        </a>
-                    </div>
-                    <div className='desktop-album-action-button'>
-                        <button className='desktop-album-action-mark-as-seen' onClick={() => {
-                            markAlbumAsSeen(album.id);
-                            toggleActionsPanel()
-                        }}>
-                            <img src='album-actions/ghost.png' alt=''/> Toggle Seen/Unseen
-                        </button>
-                    </div>
+                    <a className='desktop-album-action-button desktop-album-action-spotify' href={album.spotifyUri}>
+                        <img src='album-actions/spotify.png' alt=''/> Open in Spotify App
+                    </a>
+                    <a className='desktop-album-action-button desktop-album-action-spotify' href={album.spotifyWebPlayerUrl}>
+                        <img src='album-actions/google-chrome.png' alt=''/> Open in Spotify Web
+                    </a>
+                    <button className='desktop-album-action-button desktop-album-action-mark-as-seen' onClick={() => {
+                        markAlbumAsSeen(album.id);
+                        toggleActionsPanel()
+                    }}>
+                        <img src='album-actions/ghost.png' alt=''/> Toggle Seen/Unseen
+                    </button>
                 </div>
             </div>
         </div>
