1a32e202f1508960b6f58f914d1297a21635175d
Eric Majerus <ackmanx@users.noreply.github.com>
2019-09-13 23:01:30 -0500

    added stupid animations for my own amusement

--- client/components/action-bar/action-button.js
+++ client/components/action-bar/action-button.js
@@ -6,7 +6,7 @@ const _ActionButton = props => {
     const {children, handler, className, imagePath, imageAltText, loading} = props
 
     return (
-        <button className={`action-button ${className ? className : ''} ${loading ? 'disabled' : ''}`} onClick={handler} disabled={loading}>
+        <button className={`action-button ${className ? className : ''}`} onClick={handler} disabled={loading}>
             <img className='action-icon' src={imagePath} alt={imageAltText}/>
             {children}
         </button>
