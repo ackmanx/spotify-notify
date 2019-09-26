4244bfe2bf130fd66d37266020faf278fef48bff
Eric Majerus <ackmanx@users.noreply.github.com>
2019-09-26 00:52:47 -0500

    massively reduced css clutter

--- client/components/album/desktop/album-actions.js
+++ client/components/album/desktop/album-actions.js
@@ -3,29 +3,28 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 import {markAlbumAsSeen} from '../../../redux/actions/seen-albums'
+import {bemFactory} from '../../../utils/utils'
+
+const bem = bemFactory('desktop-album-actions')
 
 export const _DesktopAlbumActions = props => {
     const {album, markAlbumAsSeen, seenAlbums, toggleActionsPanel} = props
 
     return (
-        <div className='desktop-album-actions'>
-            <div className='desktop-album-actions-2'>
-                <div className='desktop-actions-container'>
-                    <a className='desktop-album-action-button desktop-album-action-spotify' href={album.spotifyUri}>
-                        <img src='album-actions/spotify.png' alt=''/> Open in Spotify App
-                    </a>
-                    <a className='desktop-album-action-button desktop-album-action-spotify' href={album.spotifyWebPlayerUrl}>
-                        <img src='album-actions/google-chrome.png' alt=''/> Open in Spotify Web
-                    </a>
-                    <button className='desktop-album-action-button desktop-album-action-mark-as-seen' onClick={() => {
-                        //If we don't delay this, the text will update from Seen to Unseen before the card close animation finishes
-                        setTimeout(() => markAlbumAsSeen(album.id), 125)
-                        toggleActionsPanel()
-                    }}>
-                        <img src='album-actions/ghost.png' alt=''/> {seenAlbums.includes(album.id) ? 'Mark as Unseen' : 'Mark as Seen'}
-                    </button>
-                </div>
-            </div>
+        <div className={bem()}>
+            <a className={bem('action')} href={album.spotifyUri}>
+                <img src='album-actions/spotify.png' alt=''/> Open in Spotify App
+            </a>
+            <a className={bem('action')} href={album.spotifyWebPlayerUrl}>
+                <img src='album-actions/google-chrome.png' alt=''/> Open in Spotify Web
+            </a>
+            <button className={`${bem('action')} ${bem('button')}`} onClick={() => {
+                //If we don't delay this, the text will update from Seen to Unseen before the card close animation finishes
+                setTimeout(() => markAlbumAsSeen(album.id), 125)
+                toggleActionsPanel()
+            }}>
+                <img src='album-actions/ghost.png' alt=''/> {seenAlbums.includes(album.id) ? 'Mark as Unseen' : 'Mark as Seen'}
+            </button>
         </div>
     )
 }
