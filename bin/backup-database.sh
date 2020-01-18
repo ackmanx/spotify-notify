d6676300af706f107e6ecb67dccfb3e2c6a91d38
Eric Majerus <ackmanx@users.noreply.github.com>
2020-01-17 21:31:46 -0600

    update mongodb brew command

--- bin/backup-database.sh
+++ bin/backup-database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#If you have mongodb installed locally (brew install mongodb), then you can use this to export the entire database into a JSON file
+#If you have mongodb installed locally (brew tap mongodb/brew; brew install mongodb-community), then you can use this to export the entire database into a JSON file
 #This template was generated at https://cloud.mongodb.com/v2/5d547f839ccf643d3ff713bc#clusters/commandLineTools/spotify-notify
 mongoexport \
     --host spotify-notify-shard-0/spotify-notify-shard-00-00-f6tmy.mongodb.net:27017,spotify-notify-shard-00-01-f6tmy.mongodb.net:27017,spotify-notify-shard-00-02-f6tmy.mongodb.net:27017 \
