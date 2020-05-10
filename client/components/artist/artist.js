cd5f103a016674e66f05152fed973b19b5bc8fd4
Eric Majerus <ackmanx@users.noreply.github.com>
2020-05-09 20:46:44 -0500

    hiding albums be default, showing artists with albums

--- client/components/artist/artist.js
+++ client/components/artist/artist.js
@@ -1,5 +1,5 @@
 import './artist.less'
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from 'react-redux'
 
 import {Album} from '../album/album'
@@ -9,6 +9,7 @@ import {bemFactory} from '../../utils/utils'
 const bem = bemFactory('artist')
 
 const _Artist = props => {
+    const [isExpanded, setIsExpanded] = useState(false)
     const {artist, markArtistAsSeen} = props
 
     const albums = artist.albums.filter(album => album.type === 'album')
@@ -24,21 +25,25 @@ const _Artist = props => {
     return (
         <div className={bem()}>
             <h2 className={bem('name')}>
-                <div>
+                <div onClick={() => setIsExpanded(!isExpanded)}>
                     {artist.name}
                 </div>
-                <img className={bem('select-all')} src='select-all.png' alt='hammer' onClick={() => markArtistAsSeen(artist.id)}/>
+                {isExpanded && <img className={bem('select-all')} src='select-all.png' alt='select-all' onClick={() => markArtistAsSeen(artist.id)}/>}
             </h2>
 
-            {hasAlbums && <>
-                <h3 className={bem('album-group')}>Albums</h3>
-                {albums.map(album => <Album key={album.id} album={album}/>)}
-            </>}
+            {isExpanded && (
+                <>
+                    {hasAlbums && <>
+                        <h3 className={bem('album-group')}>Albums</h3>
+                        {albums.map(album => <Album key={album.id} album={album}/>)}
+                    </>}
 
-            {hasSingles && <>
-                <h3 className={bem('album-group')}>Singles</h3>
-                {singles.map(single => <Album key={single.id} album={single}/>)}
-            </>}
+                    {hasSingles && <>
+                        <h3 className={bem('album-group')}>Singles</h3>
+                        {singles.map(single => <Album key={single.id} album={single}/>)}
+                    </>}
+                </>
+            )}
         </div>
     )
 }
