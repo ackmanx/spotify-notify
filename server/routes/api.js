7fa4fe9772f428b7e1bff6d9e03492ef902fc8f4
Eric Majerus <ackmanx@users.noreply.github.com>
2020-08-08 11:35:05 -0500

    shuffling spotify uris because that's what their endpoints need and more notes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-const {fetchAllPages} = require("../service/request-helpers");
 const debug = require('debug')(`sn:${path.basename(__filename)}`)
 
+const {shuffle} = require("../service/shuffler");
 const {ensureAuthenticated} = require('./spotify-auth')
 const {getUserData, saveUserData, Slices} = require('../db/dao')
 const {checkForUnseenAlbums} = require('../service/spotify')
@@ -119,31 +119,39 @@ router.get('/heartbeat', function (req, res) {
 
 /*
  * Playlist sandboxing
+ * All operations are on the same endpoint, just different HTTP methods
+ *
+ * First you get the playlist's tracks
+ * https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlists-tracks/
+ *
+ * After shuffling you have to clear the playlist
+ * https://developer.spotify.com/documentation/web-api/reference/playlists/replace-playlists-tracks/
+ *
+ * Then you add tracks in pages to rebuild it
+ * https://developer.spotify.com/documentation/web-api/reference/playlists/add-tracks-to-playlist/
  */
 router.get('/playlist', ensureAuthenticated, async function (req, res) {
-    //todo: this returns all playlists. we need to find the one we want to shuffle by getting its ID
-    // const usersPlaylists = await fetchAllPages(req.session.access_token, '/me/playlists?limit=50')
-
-    //todo: do a GET for that playlist
-    const playlist = await fetchAllPages(req.session.access_token, '/playlists/7at9lCk1NMBgJQI2v24fi3/tracks?fields=next,offset,items(track(name))')
+    //Get my Club music playlist
+    const playlist = await fetchAllPages(req.session.access_token, '/playlists/7at9lCk1NMBgJQI2v24fi3/tracks?fields=next,offset,items(track(uri))')
 
     //todo: save the response somewhere as a backup
-        //todo: verify you save the fields needed to re-create the same playlist if things go south
+    //todo: verify you save the fields needed to re-create the same playlist if things go south
 
-    //todo: shuffle in memory
+    //todo: it happened once where I got some nulls in here but don't know how
+    //todo: maybe need to do a check for nulls and a check for length of before/after?
+    const shuffled = shuffle(playlist)
 
-    //todo: PUT the same url used to GET the playlist
-        //todo: the request will have query param uris that's empty, so we delete the current playlist
+    //todo: PUT the same url used to GET the playlist to delete it
+    //todo: the request will have query param uris that's empty, so we delete the current playlist
 
     //todo: after the PUT is done, we can POST the same url with the new tracks
-        //todo: spotify only accepts 100 at a time, so you need to build pages to send out
+    //todo: spotify only accepts 100 at a time, so you need to build pages to send out
 
     //todo: retry handling needs to work for this because otherwise we might delete the playlist without being able to recreate it shuffled
 
     //todo: by now we should have the backup saved and our playlist shuffled
 
-    // res.json(usersPlaylists)
-    res.json(playlist)
+    res.json({uris: shuffled})
 })
 
 module.exports = router
--- server/service/shuffler.js
+++ server/service/shuffler.js
@@ -1,33 +1,37 @@
+const path = require('path')
 const debug = require('debug')(`sn:${path.basename(__filename)}`)
 
 exports.shuffle = function shuffle(playlist) {
     /*
-     * Generate flat list of names
+     * Generate flat list of tracks
      */
-    let names = []
+    let tracks = []
 
     playlist.forEach(playlistPage => {
-        names = names.concat(playlistPage.items.map(item => item.track.name))
+        tracks = tracks.concat(playlistPage.items.map(item => item.track.uri))
     })
 
     /*
-     * Now we have a flat array of the names, we can shuffle it
+     * Now we have a flat array of the tracks, we can shuffle it
      * Let's sort this so we can more easily see progress while developing
      */
-    names.sort()
+    tracks.sort()
 
     /*
      * Shuffle
      * Go through each element
      * Get a random new index number
      * Swap
-     *
      */
-    for (let i = 0; i < names.length; i++) {
-        const newIndex = Math.round(Math.random() * names.length)
+    for (let i = 0; i < tracks.length; i++) {
+        const newIndex = Math.round(Math.random() * tracks.length)
 
-        const elementBeingSwapped = names[newIndex]
-        names[newIndex] = names[i]
-        names[i] = elementBeingSwapped
+        const trackBeingReplaced = tracks[newIndex]
+        tracks[newIndex] = tracks[i]
+        tracks[i] = trackBeingReplaced
+
+        debug('new:', tracks[newIndex], 'old:', tracks[i])
     }
+
+    return tracks
 }
