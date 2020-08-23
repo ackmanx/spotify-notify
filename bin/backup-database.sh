10f505596d7314bb387b5030070ee80b68bc8bc4
Eric Majerus <ackmanx@users.noreply.github.com>
2020-08-23 15:31:43 -0500

    updated backup script to work with heroku again

--- bin/backup-database.sh
+++ bin/backup-database.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-#If you have mongodb installed locally (brew tap mongodb/brew; brew install mongodb-community), then you can use this to export the entire database into a JSON file
-#This template was generated at https://cloud.mongodb.com/v2/5d547f839ccf643d3ff713bc#clusters/commandLineTools/spotify-notify
+#If you have mongodb tools installed (brew tap mongodb/brew; brew install mongodb-database-tools), then you can use this to export the entire database into a JSON file
+#This template was generated at https://cloud.mongodb.com/v2/5d547f839ccf643d3ff713bc#clusters/commandLineTools/hobby-cluster
 mongoexport \
-    --host spotify-notify-shard-0/spotify-notify-shard-00-00-f6tmy.mongodb.net:27017,spotify-notify-shard-00-01-f6tmy.mongodb.net:27017,spotify-notify-shard-00-02-f6tmy.mongodb.net:27017 \
-    --ssl \
-    --username admin \
-    --password F8cHHZjWza0SRGOx \
-    --authenticationDatabase admin \
-    --db spotify-notify \
+    --uri mongodb+srv://admin:F8cHHZjWza0SRGOx@hobby-cluster-f6tmy.mongodb.net/spotify-notify \
     --collection user-data \
+    --type json \
     --out "../../db-backups/spotify-notify-$(date '+%Y-%m-%d').json"
